8960df1571e034c71e116473f1d094f38685063a
ahstevens <astevens@ccom.unh.edu>
2023-11-30 10:01:16 -0500

    Update DemoControls.cs

--- Assets/Scripts/DemoControls.cs
+++ Assets/Scripts/DemoControls.cs
@@ -27,33 +27,78 @@ public class DemoControls : MonoBehaviour
         if (PlayerPrefs.HasKey($"{SceneManager.GetActiveScene().name} Forward Position"))
             coralPos.z = PlayerPrefs.GetFloat($"{SceneManager.GetActiveScene().name} Forward Position");
 
-        coralModel.transform.position = coralPos;
+        if (PlayerPrefs.HasKey($"{SceneManager.GetActiveScene().name} Vertical Position"))
+            coralPos.y = PlayerPrefs.GetFloat($"{SceneManager.GetActiveScene().name} Vertical Position");
+
+        float coralRot = 0F;
+
+        if (PlayerPrefs.HasKey($"{SceneManager.GetActiveScene().name} Rotation"))
+            coralRot = PlayerPrefs.GetFloat($"{SceneManager.GetActiveScene().name} Rotation");
+
+        coralModel.transform.SetPositionAndRotation(coralPos, Quaternion.Euler(0F, coralRot, 0F));
     }
 
     // Update is called once per frame
     void Update()
     {
         bool shiftPressed = Keyboard.current.shiftKey.isPressed;
+        bool ctrlPressed = Keyboard.current.ctrlKey.isPressed;
 
         if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
         {
-            coralModel.transform.position += Vector3.right * (shiftPressed ? 0.1f : 0.01f);
-            PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Side Position", coralModel.transform.position.x);
+            if (ctrlPressed)
+            {
+                var rot = coralModel.transform.rotation.eulerAngles;
+                rot.y += (shiftPressed ? 10f : 1f);
+                coralModel.transform.rotation = Quaternion.Euler(rot);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Rotation", coralModel.transform.rotation.eulerAngles.y);
+            }
+            else
+            {
+                coralModel.transform.position += Vector3.right * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Side Position", coralModel.transform.position.x);
+            }
         }
         if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
         {
-            coralModel.transform.position += Vector3.left * (shiftPressed ? 0.1f : 0.01f);
-            PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Side Position", coralModel.transform.position.x);
+            if (ctrlPressed)
+            {
+                var rot = coralModel.transform.rotation.eulerAngles;
+                rot.y -= (shiftPressed ? 10f : 1f);
+                coralModel.transform.rotation = Quaternion.Euler(rot);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Rotation", coralModel.transform.rotation.eulerAngles.y);
+            }
+            else
+            {
+                coralModel.transform.position += Vector3.left * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Side Position", coralModel.transform.position.x);
+            }
         }
         if (Keyboard.current.upArrowKey.wasPressedThisFrame)
         {
-            coralModel.transform.position += Vector3.forward * (shiftPressed ? 0.1f : 0.01f);
-            PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Forward Position", coralModel.transform.position.z);
+            if (ctrlPressed)
+            {
+                coralModel.transform.position += Vector3.up * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Vertical Position", coralModel.transform.position.y);
+            }
+            else
+            {
+                coralModel.transform.position += Vector3.forward * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Forward Position", coralModel.transform.position.z);
+            }
         }
         if (Keyboard.current.downArrowKey.wasPressedThisFrame)
         {
-            coralModel.transform.position += Vector3.back * (shiftPressed ? 0.1f : 0.01f);
-            PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Forward Position", coralModel.transform.position.z);
+            if (ctrlPressed)
+            {
+                coralModel.transform.position -= Vector3.up * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Vertical Position", coralModel.transform.position.y);
+            }
+            else
+            {
+                coralModel.transform.position += Vector3.back * (shiftPressed ? 0.1f : 0.01f);
+                PlayerPrefs.SetFloat($"{SceneManager.GetActiveScene().name} Forward Position", coralModel.transform.position.z);
+            }
         }
 
         if (Keyboard.current.digit1Key.wasPressedThisFrame)
@@ -63,6 +108,10 @@ public class DemoControls : MonoBehaviour
         if (Keyboard.current.digit2Key.wasPressedThisFrame)
         {
             StartCoroutine(LoadNewScene("Coral Reef"));
+        }
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            StartCoroutine(LoadNewScene("UNH Site 4 2020"));
         }
 
         if (Keyboard.current.rKey.wasPressedThisFrame)
